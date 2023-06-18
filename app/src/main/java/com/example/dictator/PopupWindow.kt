225feab59475347750447188a08f3352ed5619ec
Ritik <soniritik105@gmail.com>
2023-06-19 00:37:49 +0530

    text edited

--- app/src/main/java/com/example/dictator/PopupWindow.kt
+++ app/src/main/java/com/example/dictator/PopupWindow.kt
@@ -1,6 +1,5 @@
 package com.example.dictator
 
-import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -9,16 +8,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.DialogFragment
-import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
-import kotlinx.coroutines.CompletableDeferred
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import org.json.JSONArray
-import org.json.JSONObject
-import java.util.concurrent.CountDownLatch
 
 class PopupWindow : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
